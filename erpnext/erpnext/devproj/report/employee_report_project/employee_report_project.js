0764f5a615ade757616a9e32abfae38607e4d155
faiz <faizrizky@ymail.com>
2023-08-08 04:48:06 +0000

    fix employee_report_project penambahan kolom days dan hapus prefix pada kolom task, penambahan exp_end_date dan actual_end_date pada print report

--- erpnext/erpnext/devproj/report/employee_report_project/employee_report_project.js
+++ erpnext/erpnext/devproj/report/employee_report_project/employee_report_project.js
@@ -10,15 +10,49 @@ frappe.query_reports["Employee Report Project"] = {
 			fieldtype: "Link",
 			options: "Project",
 			default: frappe.defaults.get_user_default("Project"),
-			reqd: 0
+			reqd: 0,
+			custom_values_after: function (filter) {
+				var project = filter["project_name"];
+				var expected_end_date = "";
+				var actual_end_date = "";
+				var concatenated_str = "";
+
+				if (project) {
+					frappe.call({
+						async: false,
+						freeze: true,
+						method: "frappe.client.get_value",
+						args: {
+							doctype: "Project",
+							filters: { name: project },
+							fieldname: ["expected_end_date", "actual_end_date"]
+						},
+						callback: function (response) {
+							if (response.message) {
+								expected_end_date = response.message.expected_end_date;
+								actual_end_date = response.message.actual_end_date;
+
+								if (expected_end_date) {
+									concatenated_str += "<br><h6 class='text-secondary'>Expected End Date: " + expected_end_date + "</h6>";
+								}
+
+								if (actual_end_date) {
+									concatenated_str += "<h6 class='text-secondary'>Actual End Date: " + actual_end_date + "</h6>";
+								}
+							}
+						}
+					});
+				}
+				return concatenated_str;
+			},
 		},
 		{
 			fieldname: "branch",
 			label: __("Position"),
 			fieldtype: "Link",
 			options: "Branch",
-			// default: frappe.defaults.get_user_default("Project"),
 			reqd: 0
-		},
+		}
 	]
 };
+
--- erpnext/erpnext/devproj/report/employee_report_project/employee_report_project.py
+++ erpnext/erpnext/devproj/report/employee_report_project/employee_report_project.py
@@ -22,6 +22,7 @@ def get_column():
         {"fieldname": "employee_name", "fieldtype": "", "label": _("User"), "height": 150, "width": 200},
         {"fieldname": "branch", "fieldtype": "", "label": _("Team"), "height": 150, "width": 320, "align": "left"},
         {"fieldname": "task_name", "fieldtype": "", "label": _("Task (Days)"), "height": 500, "width": 350},
+        {"fieldname": "days", "fieldtype": "", "label": _("Days"), "height": 500, "width": 150},
         {"fieldname": "task_taken", "fieldtype": "", "label": _("Task Taken")},
         {"fieldname": "total_weight", "fieldtype": "", "label": _("Total Weight"), "width": 120, "height": 150},
         {"fieldname": "total_days", "fieldtype": "", "label": _("Total Days"), "height": 150, "width": 150,
@@ -44,12 +45,14 @@ def get_data(filters):
     for row in result:
         parent_doc = frappe.get_doc("SD Data Report", row.name)
         concatenated_str = ""
+        concatenated_days = ""
         if hasattr(parent_doc, "task") and parent_doc.task:
             child_records = parent_doc.get("task")
-            total_days = 0
+            total_days = parent_doc.total_days
             total_weight = 0
             task_taken = len(child_records)
             concatenated_str += "<ol style='padding-left: 15px;'>"
+            # concatenated_days += "<ol style='padding-left: 15px;'>"
 
             prev_task_name = None  # Variable to store the previous task name
             first_record = True
@@ -59,25 +62,46 @@ def get_data(filters):
                 task_days = str(child.days)
                 total_weight += child.task_weight
 
-                if prev_task_name and prev_task_name.split('-')[0].strip() != task_name.split('-')[0].strip():
-                    total_days += child.days
+                pname = task_name.split('-')[0].strip()
 
+                if prev_task_name and prev_task_name.split('-')[0].strip() != task_name.split('-')[0].strip():
+                    # total_days += child.days
+                    subject,exp_start_date, review_date = frappe.db.get_value('Task', pname, ['subject','exp_start_date', 'review_date'])
+                    # print("subject : ", subject, " || ","exp_start_date : ", exp_start_date," || ","review_date : ",review_date)
+
+                    # print("NAMA TASK : ", pname)
+
+                # print("TOTAL STRING : ", task_name + " || " + str(len(task_name)/70))
+                # if (len(task_name)/70) >=1.5 :
+                #     concatenated_days += task_days  + "<br />" + "<br />" + "<br />" + "<br />"
+                # elif (len(task_name)/70) >=1:
+                #     concatenated_days += task_days  + "<br />" + "<br />" + "<br />"
+                # elif (len(task_name)/70) >=0.5:
+                #     concatenated_days += task_days  + "<br />" + "<br />"
+                # else:
+                #     concatenated_days += task_days  + "<br />"
+                concatenated_days += task_days  + "<br />" + "<br />"
                 if child.is_parent == 0:
-                    concatenated_str += "<li>" + task_name + " ( " + task_days + " Days ) " + "<br />"
+                    # concatenated_str += "<li>" + task_name + " ( " + task_days + " Days ) " + "<br />"
+                    concatenated_str += "<li>" + task_name + "<br />"
+                    
+                    
                 else:
-                    concatenated_str += "<li>" + task_name + " - Integration" + " ( " + task_days + " Days ) " + "<br />"
+                    # concatenated_str += "<li>" + task_name + " - Integration" + " ( " + task_days + " Days ) " + "<br />"
+                    concatenated_str += "<li>" + task_name + " - Integration" + "<br />"
 
                 prev_task_name = task_name
 
                 if first_record:
-                    total_days += child.days
+                    # total_days += child.days
                     first_record = False
-
+    
             data.append({
                 "total_weight": row.total_weight,
                 "employee_name": row.employee_name,
                 "branch": row.branch,
                 "task_taken": task_taken,
+                "days": concatenated_days,
                 "task_name": concatenated_str,
                 "total_days": total_days,
                 "total_weight": total_weight

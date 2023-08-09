ea843624c1c41ab3ff332fce38aee555b14ba661
faiz <faizrizky@ymail.com>
2023-08-09 06:40:09 +0000

    penambahan return function actual end date dan expectation end date pada print report

--- erpnext/erpnext/devproj/report/project_department_report/project_department_report.js
+++ erpnext/erpnext/devproj/report/project_department_report/project_department_report.js
@@ -4,21 +4,47 @@
 
 frappe.query_reports["Project Department Report"] = {
 	"filters": [
-		// {
-		// 	"fieldname": "company",
-		// 	"label": __("Company"),
-		// 	"fieldtype": "Link",
-		// 	"options": "Company",
-		// 	"default": frappe.defaults.get_user_default("Company"),
-		// 	"reqd": 1
-		// },
 		{
 			fieldname: "project",
 			label: __("Project"),
 			fieldtype: "Link",
 			options: "Project",
-			// default: frappe.defaults.get_user_default("Project"),
-			reqd: 0
+			default: frappe.defaults.get_user_default("Project"),
+			reqd: 0,
+			custom_values_after: function (filter) {
+				var project = filter["project"];
+				var expected_end_date = "";
+				var actual_end_date = "";
+				var concatenated_str = "";
+
+				if (project) {
+					frappe.call({
+						async: false,
+						freeze: true,
+						method: "frappe.client.get_value",
+						args: {
+							doctype: "Project",
+							filters: { name: project },
+							fieldname: ["expected_end_date", "actual_end_date"]
+						},
+						callback: function (response) {
+							if (response.message) {
+								expected_end_date = response.message.expected_end_date;
+								actual_end_date = response.message.actual_end_date;
+
+								if (expected_end_date) {
+									concatenated_str += "<br><h6 class='text-secondary'>End Date: " + expected_end_date + "</h6>";
+								}
+
+								if (actual_end_date) {
+									concatenated_str += "<h6 class='text-secondary'>Actual End Date: " + actual_end_date + "</h6>";
+								}
+							}
+						}
+					});
+				}
+				return concatenated_str;
+			},
 		},
 		// {
 		// 	"fieldname": "is_active",
